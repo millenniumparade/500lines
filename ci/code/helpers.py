ed0dc32f01dcc4473aa1c2163f08d5ef41c83ed8
millennumparade <liuyl2022@gmail.com>
2020-07-05 20:11:36 -0400

    update socketserver etc.

--- ci/code/helpers.py
+++ ci/code/helpers.py
@@ -3,7 +3,7 @@ import socket
 def communicate(host, port, request):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
-    s.send(request)
+    s.send(str.encode(request))
     response = s.recv(1024)
     s.close()
-    return response
+    return response.decode()
--- ci/code/test_runner.py
+++ ci/code/test_runner.py
@@ -13,23 +13,24 @@ import errno
 import os
 import re
 import socket
-import SocketServer
+import socketserver
 import subprocess
 import time
 import threading
 import unittest
-
+import logging
 import helpers
 
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s -  %(levelname)s -  %(message)s')
 
-class ThreadingTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+class ThreadingTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     dispatcher_server = None # Holds the dispatcher server host/port information
     last_communication = None # Keeps track of last communication from dispatcher
     busy = False # Status flag
     dead = False # Status flag
 
 
-class TestHandler(SocketServer.BaseRequestHandler):
+class TestHandler(socketserver.BaseRequestHandler):
     """
     The RequestHandler class for our server.
     """
@@ -39,35 +40,35 @@ class TestHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
-        command_groups = self.command_re.match(self.data)
+        command_groups = self.command_re.match(self.data.decode())
         command = command_groups.group(1)
         if not command:
-            self.request.sendall("Invalid command")
+            self.request.sendall(str.encode("Invalid command"))
             return
         if command == "ping":
-            print "pinged"
+            logging.info("pinged")
             self.server.last_communication = time.time()
-            self.request.sendall("pong")
+            self.request.sendall(str.encode("pong"))
         elif command == "runtest":
-            print "got runtest command: am I busy? %s" % self.server.busy
+            logging.info("got runtest command: am I busy? %s" % self.server.busy)
             if self.server.busy:
-                self.request.sendall("BUSY")
+                self.request.sendall(str.encode("BUSY"))
             else:
-                self.request.sendall("OK")
-                print "running"
+                self.request.sendall(str.encode("OK"))
+                logging.info("running")
                 commit_id = command_groups.group(2)[1:]
                 self.server.busy = True
                 self.run_tests(commit_id,
                                self.server.repo_folder)
                 self.server.busy = False
         else:
-            self.request.sendall("Invalid command")
+            self.request.sendall(str.encode("Invalid command"))
 
     def run_tests(self, commit_id, repo_folder):
         # update repo
         output = subprocess.check_output(["./test_runner_script.sh",
                                         repo_folder, commit_id])
-        print output
+        logging.info(output)
         # run the tests
         test_folder = os.path.join(repo_folder, "tests")
         suite = unittest.TestLoader().discover(test_folder)
@@ -110,8 +111,8 @@ def serve():
             try:
                 server = ThreadingTCPServer((runner_host, runner_port),
                                             TestHandler)
-                print server
-                print runner_port
+                logging.info(server)
+                logging.info(runner_port)
                 break
             except socket.error as e:
                 if e.errno == errno.EADDRINUSE:
@@ -149,11 +150,11 @@ def serve():
                                        int(server.dispatcher_server["port"]),
                                        "status")
                     if response != "OK":
-                        print "Dispatcher is no longer functional"
+                        logging.info("Dispatcher is no longer functional")
                         server.shutdown()
                         return
                 except socket.error as e:
-                    print "Can't communicate with dispatcher: %s" % e
+                    logging.info("Can't communicate with dispatcher: %s" % e)
                     server.shutdown()
                     return
 
